795f7afbb9d8ee485820b92e4beea93288c34579
Nicola Paolucci <nick@durdn.com>
2012-12-06 18:01:39 +0100

    vagrant destroy && vagrant up now creates the entire system correctly

--- manifests/default.pp
+++ manifests/default.pp
@@ -11,8 +11,10 @@ class must-have {
              "screen",
              "tmux",
              "curl",
+             "libxslt1-dev",
              "bash"]:
     ensure => present,
+    require => Exec["apt-get update"],
   }
 
   exec {
@@ -21,7 +23,7 @@ class must-have {
     cwd => "/home/vagrant",
     user => "vagrant",
     path    => "/usr/bin/:/bin/",
-    require => Package["curl"],
+    require => Package["curl", "git-core"],
     logoutput => true,
     creates => "/home/vagrant/.cfg",
   }
@@ -40,44 +42,41 @@ class must-have {
 } 
 
 include must-have
-include apt
-
-apt::ppa { "ppa:chris-lea/node.js": }
-
-include nodejs
-
-package { "coffee-script":
-  ensure   => "1.4.0",
-  provider => "npm",
-  require => Package["npm"],
-}
 
 rbenv::install { "vagrant":
   group => "vagrant",
   home  => "/home/vagrant",
+  require => Exec["apt-get update"],
 }
 
 rbenv::compile { "1.9.3-p327":
   user => "vagrant",
   home => "/home/vagrant",
   global => true,
+  require => Rbenv::Install["vagrant"],
 }
 
-exec {
-  "install_eco_source":
-  command => "sudo gem install eco-source --pre",
-  provider => "shell",
-  cwd => "/home/vagrant",
+rbenv::gem { ["eco-source"]:
   user => "vagrant",
-  /*path    => "/usr/bin/:/bin/",*/
-  logoutput => "true",
+  ruby => "1.9.3-p327",
   before => Rbenv::Gem["eco"],
+  install_params => "--pre",
 }
 
-rbenv::gem { ["eco", "builder", "RedCloth", "markaby", "liquid", "less", "radius", "nokogiri", "creole"]:
+rbenv::gem { ["eco", "builder", "RedCloth", "markaby", "liquid", "less", "radius", "nokogiri", "creole", "ejs"]:
   user => "vagrant",
   ruby => "1.9.3-p327",
   before => Rbenv::Gem["middleman"],
+}
+
+include apt
+apt::ppa { "ppa:chris-lea/node.js": }
+include nodejs
+
+package { "coffee-script":
+  ensure   => "1.4.0",
+  provider => "npm",
+  require => Package["npm"],
 }
 
 rbenv::gem { "middleman":
