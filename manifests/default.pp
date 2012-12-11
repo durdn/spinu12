b7b66604b4efde46bd54501dd01343e3e154d906
Nicola Paolucci <nick@durdn.com>
2012-12-11 23:03:10 +0100

    Removed unneeded sub-modules install step

--- manifests/default.pp
+++ manifests/default.pp
@@ -28,19 +28,6 @@ class must-have {
     creates => "/home/vagrant/.cfg",
   }
 
-  exec {
-    "install_submodules":
-    command => "cd /home/vagrant/.cfg && git submodule update --init",
-    provider => "shell",
-    cwd => "/home/vagrant/.cfg",
-    user => "vagrant",
-    path    => "/usr/bin/:/bin/",
-    require => Exec["install_cfg"],
-    logoutput => "true",
-    creates => "/home/vagrant/.cfg/.vim/bundle/scrooloose-nerdtree",
-  }
-} 
-
 include must-have
 
 rbenv::install { "vagrant":
