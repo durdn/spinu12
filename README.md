3d147888af890a30556214993ab4e91ea56e9c1c
Nicola Paolucci <nick@durdn.com>
2013-01-02 12:45:27 +0100

    Vagrant setup for Atlassian Stash

--- README.md
+++ README.md
@@ -1,17 +1,27 @@
-# Vagrant setups for development
+# Vagrant setup for Atlassian Stash
 
-These are the 2012 Vagrant configurations for my development virtual machines. Explore branches for various setups.
+http://www.atlassian.com/software/stash/overview
 
 ## Install with
 
+- Install Vagrant: http://www.vagrantup.com
+
+- Execute:
+
     vagrant box add base http://files.vagrantup.com/precise32.box
 
-    git clone https://github.com/durdn/spinu12.git
-    
-    cd spinu12
+    git clone https://github.com/durdn/spinu12.git stash
+
+    cd stash
+
+    git co -b stash origin/stash
     
     vagrant up
     
 ## Enjoy with
 
-    vagrant ssh
+    cd /vagrant
+
+    STASH_HOME=/vagrant/stash-home /vagrant/atlassian-stash-2.0.1/bin/start-stash.sh
+
+Access local browser at http://localhost:7990/setup
--- manifests/default.pp
+++ manifests/default.pp
@@ -1,6 +1,18 @@
+define append_if_no_such_line($file, $line, $refreshonly = 'false') {
+   exec { "/bin/echo '$line' >> '$file'":
+      unless      => "/bin/grep -Fxqe '$line' '$file'",
+      path        => "/bin",
+      refreshonly => $refreshonly,
+   }
+}
+
 class must-have {
+  include apt
+  apt::ppa { "ppa:webupd8team/java": }
+
   exec { 'apt-get update':
-    command => '/usr/bin/apt-get update'
+    command => '/usr/bin/apt-get update',
+    require => Apt::Ppa["ppa:webupd8team/java"],
   }
 
   package { ["vim",
@@ -8,9 +20,20 @@ class must-have {
              "tmux",
              "curl",
              "git-core",
-             "bash"]:
+             "bash",
+             "oracle-java7-installer"]:
     ensure => present,
     require => Exec["apt-get update"],
+  }
+
+  exec {
+    "accept_license":
+    command => "echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections && echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections",
+    cwd => "/home/vagrant",
+    user => "vagrant",
+    path    => "/usr/bin/:/bin/",
+    before => Package["oracle-java7-installer"],
+    logoutput => true,
   }
 
   exec {
@@ -23,6 +46,33 @@ class must-have {
     logoutput => true,
     creates => "/home/vagrant/.cfg",
   }
-} 
+
+  exec {
+    "download_stash":
+    command => "curl -L http://www.atlassian.com/software/stash/downloads/binary/atlassian-stash-2.0.1.tar.gz | tar zx",
+    cwd => "/vagrant",
+    user => "vagrant",
+    path    => "/usr/bin/:/bin/",
+    require => Exec["install_cfg"],
+    logoutput => true,
+    creates => "/vagrant/atlassian-stash-2.0.1",
+  }
+
+  exec {
+    "create_stash_home":
+    command => "mkdir -p /vagrant/stash-home",
+    cwd => "/vagrant",
+    user => "vagrant",
+    path    => "/usr/bin/:/bin/",
+    require => Exec["download_stash"],
+    logoutput => true,
+    creates => "/vagrant/stash-home",
+  }
+
+  append_if_no_such_line{ motd:
+    file => "/etc/motd",
+    line => "Run Stash with: STASH_HOME=/vagrant/stash-home /vagrant/atlassian-stash-2.0.1/bin/start-stash.sh"
+  }
+}
 
 include must-have
